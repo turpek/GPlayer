35d122b2fab54b252365989603ca2a5420544d9b
Guilherme <gui.faria.96@hotmail.com>
2024-08-23 16:35:43 -0300

    Removido o suporte para Queue.

--- src/utils.py
+++ src/utils.py
@@ -1,47 +1,46 @@
 from src.buffer_error import VideoOpenError
 from cv2 import VideoCapture
-from multiprocessing import Pipe, Lock, SimpleQueue
+from multiprocessing import Pipe, shared_memory
 from pathlib3x import Path
 from time import time
 
 import cv2
+import numpy as np
 import pickle
 import traceback
 
 
+READINESS = 0
+READING = 1
+CLEANING = 2
+EXCEPTION = 3
+
+
 def reader(filename: str,
-           queue: SimpleQueue,
-           lock: Lock,
+           conn: Pipe,
            buffersize: int,
-           bufferlog: bool,
-           conn: Pipe) -> None:
+           bufferlog: bool) -> None:
     """Um invólucro que chama a função responsável por ler os frames através do modulo da Opencv.
 
         Args:
             filename (str): caminho até o arquivo de mídeo.
-            queue (SimpleQueue): uma fila onde serão armazenadas os frames.
-            lock (Lock): objeto para travar a queue na gravação do frame na mesma.
+            conn (Pipe): canal para comunicação entre os dois processos.
             buffersize (int): número de frames a serem armazenadas na queue.
             bufferlog (bool): ativa o log.
-            conn (Pipe): canal para comunicação entre os dois processos.
 
     """
 
     def reader_task(cap: VideoCapture,
-                    queue: SimpleQueue,
-                    lock: Lock,
+                    conn: Pipe,
                     buffersize: int,
-                    bufferlog: bool,
-                    conn: Pipe) -> int:
+                    bufferlog: bool) -> int:
         """Função responsável por ler os frames através do modulo da Opencv.
 
             Args:
                 cap (VideoCapture): objeto usado para gerar os frames.
-                queue (SimpleQueue): uma fila onde serão armazenadas os frames.
-                lock (Lock): objeto para travar a queue na gravação do frame na mesma.
+                conn (Pipe): canal para comunicação entre os dois processos.
                 buffersize (int): número de frames a serem armazenadas na queue.
                 bufferlog (bool): ativa o log.
-                conn (Pipe): canal para comunicação entre os dois processos.
 
             Returns:
                 int: retorna o frame_id do 1o. frame a ser lido no próximo ciclo
@@ -64,17 +63,25 @@ def reader(filename: str,
         elif check_frame_id != frame_id:
             cap.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
 
+        # Enviando o sinal de leitura do frames
+        conn.send(READING)
+        shm_list = list()
+
         # Bloco onde os frames são lidos e armazenados na fila
         while True:
 
+            print(frame_id)
             if frame_id in lot:
                 ret, frame = cap.read()
                 if ret:
-                    # with lock:
-                    # Deve-se colocar o indice do frame + frame na queue
-                    print('Colocando', ret, queue)
-                    queue.put((frame_id))
-                    print('Colocou')
+                    shm_name = f'frame_id={frame_id}'
+                    shm = shared_memory.SharedMemory(name=shm_name,
+                                                     create=True,
+                                                     size=frame.nbytes)
+                    shared_frame = np.ndarray(frame.shape, dtype=frame.dtype, buffer=shm.buf)
+                    shared_frame[:] = frame[:]
+                    conn.send((frame_id, shm_name, frame.shape, frame.dtype))
+                    shm_list.append(shm)
                     qsize += 1
             else:
                 print('grab')
@@ -83,14 +90,25 @@ def reader(filename: str,
             if bufferlog:
                 print(qsize, qsize, frame_id)
             if qsize == buffersize:
+                print('Saindo normal')
                 break
             frame_id += 1
 
-            if bufferlog:
-                end = time()
-                count = frame_id - start_frame
-                print(f'\nLidos {count} em {end - start}s')
-                print(f'{count / (end - start):.2f} FPS')
+        if bufferlog:
+            end = time()
+            count = frame_id - start_frame
+            print(f'\nLidos {count} em {end - start}s')
+            print(f'{count / (end - start):.2f} FPS')
+
+        # Enviando o sinal para a limpeza
+        conn.send(CLEANING)
+        clean_flag = conn.recv()
+        if clean_flag is True:
+            for shm in shm_list:
+                print(f'filho fechou {shm.name}')
+                shm.close()
+                shm.unlink()
+        conn.send(READINESS)
 
         return frame_id
 
@@ -106,6 +124,7 @@ def reader(filename: str,
 
         # Bloco "príncipal" da função, que é responsavel por ativar a leitura dos
         # frames por meio da função reader_task.
+        conn.send(READINESS)
         while True:
 
             # O pai deve enviar um bool para task_flag, se a mesma for True
@@ -113,7 +132,7 @@ def reader(filename: str,
             # task_flag não for bool, uma exceção deve ser levantada.
             task_flag = conn.recv()
             if task_flag is True:
-                reader_task(cap, queue, lock, buffersize, bufferlog, conn)
+                reader_task(cap, conn, buffersize, bufferlog)
             elif task_flag is False:
                 break
             else:
@@ -121,9 +140,14 @@ def reader(filename: str,
 
     except Exception as e:
         exc_info = traceback.format_exc()
+
+        print(e)
+        # Enviando o sinal de exceção!
+        conn.send(EXCEPTION)
         conn.send(pickle.dumps((e, exc_info)))
 
     finally:
         if cap is not None:
             cap.release()
+        print('fILHO FOI FECHADO...')
         conn.close()
