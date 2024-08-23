7442ec7fd9480a47ebd1881e354f52b5581daae9
Guilherme <gui.faria.96@hotmail.com>
2024-08-23 16:32:41 -0300

    Adicionado novos erros

--- src/buffer_error.py
+++ src/buffer_error.py
@@ -1,3 +1,8 @@
 class VideoBufferError(Exception):
     def __init__(self, message):
         self.message = message
+
+
+class VideoOpenError(Exception):
+    def __init__(self, message):
+        self.message = message
