49ebecdadf86431ffeef0928ea086e96a62f5939
Guilherme <gui.faria.96@hotmail.com>
2024-08-28 20:52:38 -0300

    A classe VideoBufferRight foi remodelado para suportar a Threads ao invez de Process, também foi adicionado um atributo para fazer a checagem do estado do buffer e feito pequenas melhoras.

--- src/buffer_right.py
+++ src/buffer_right.py
@@ -10,64 +10,89 @@ módulo da opencv, a mesma tem a seguinte estrutura:
         +sequence_frames_ord: lista que fornece os frames a serem lidos.
         +sequence_frames: dicionário que mapeia os frames a serem lidos.
         +process: Processo que roda em paralelo com o programa principal, na qual os frames serão lidos
-        -_end_frame: inteiro que armazena o id do último frame a ser lido
         -_start_frame: inteiro que armazenao o id do primeiro frame a ser lido
         +parent_conn: pipe para comunicação com o processo descrito acima
         +child_conn: pipe passado para o processo descrito acima
 
         +start_frame() int: metodo que retorna o id do primeiro frame a ser lido
-        +end_frame() int: metodo que retorna o id do último frame a ser lido
 
 """
 
 
 from array import array
-from multiprocessing import Pipe, Process
+from cv2 import VideoCapture
 from numpy import ndarray
+from queue import Queue
 from src.buffer_error import VideoBufferError
-from src.my_structure import Queue
-from src.utils import reader
+from src.checkout import Checkout
+from src.my_structure import MyQueue
+from src.channel import Channel
+from src.utils import reader, BUFFER_RIGHT
+from threading import Event, Thread, Lock
+from time import sleep
 
 import bisect
-from src.utils import READINESS
-# import ipdb
+import ipdb
+
+lock = Lock()
+
+event = Event()
 
 
-class VideoBufferRight(Queue):
+class VideoBufferRight(MyQueue):
     """Classe que implementa o buffer dos frames a serem lidos"""
 
     def __init__(self,
-                 path: str,
-                 sequence_frames: list[int],
-                 buffersize=25, *,
+                 cap: VideoCapture,
+                 sequence_frames: list[int], *,
+                 buffersize=25,
                  bufferlog=False,
                  name='buffer'):
 
-        # Criando os pipe para comunicação entre os processos
-        parent_conn, child_conn = Pipe()
+        super().__init__(maxsize=buffersize)
+
+        # Criando as queues  para comunicação entre as threads
+        parent_conn, child_conn = Channel()
         self.parent_conn = parent_conn
         self.child_conn = child_conn
-
-        super().__init__(parent_conn, maxsize=buffersize)
+        self.check = Checkout(parent_conn, event, lock)
 
         # Definições das variaveis que lidam com o Thread
-        self.path = path
+        self.cap = cap
         self.name = name
         self.buffersize = buffersize
         self.bufferlog = bufferlog
 
         # Definições das variaveis responsavel pela criação do buffer
         self._start_frame = None
-        self._old_frame = None
+        self.frame_id = None
         self.__store_frame_id = None
+        self._set_frame = None
 
         # Atributos usados para determinar os frames que serao armazenados no buffer
         self.lot = list()
         self.lot_mapping = set()
         self.set_lot(sequence_frames)
 
-        self.process = None
+        self.thread = None
+        self.no_block = True
         self.delay = 0.005
+
+    def __del__(self):
+        if self.thread is not None:
+            while event.is_set():
+                sleep(0.1)
+                event.clear()
+            self.parent_conn.send(False)
+        self.parent_conn.close()
+
+    def frame_id(self) -> int | None:
+        """Metod que retorna o id do ultimo frame lido pelo metodo read.
+
+            Returns:
+                int|None: id do último frame lido, caso seja None, nenhum frame foi lido ainda
+        """
+        return self._frame_id
 
     def first_frame(self) -> int:
         """Devolve o primeiro frame do lote.
@@ -85,7 +110,7 @@ class VideoBufferRight(Queue):
         """
         return self.lot[-1]
 
-    def start_frame(self) -> int|bool:
+    def start_frame(self) -> int | bool:
         """Define o primeiro frame a ser lido em cada novo ciclo do buffer.
 
             Quando usado o metodo put, start_frame pode assumir o valor de False,
@@ -100,7 +125,6 @@ class VideoBufferRight(Queue):
         """
         if self._start_frame is None:
             self._start_frame = self.lot[0]
-            self._start_frame = self.lot[0]
         return self._start_frame
 
     def set_lot(self, lot: list[int]) -> None:
@@ -111,7 +135,6 @@ class VideoBufferRight(Queue):
         """
         self.lot = array('l', sorted(lot))
         self.lot_mapping = set(lot)
-        self._old_frame = self.lot[0]
 
     def set(self, frame_id: int) -> None:
         """Coloco o frame_id como start_frame no próximo ciclo de leitura dos frames.
@@ -131,8 +154,7 @@ class VideoBufferRight(Queue):
             except IndexError:
                 raise IndexError('frame_id does not belong to the lot range.')
 
-        self._start_frame = frame_id
-        self._old_frame = None
+        self._set_frame = frame_id
 
     def put(self, frame_id: int, frame: ndarray) -> None:
         """Método usado para encher o buffer de maneira manual e de forma segura.
@@ -141,40 +163,67 @@ class VideoBufferRight(Queue):
                 frame_id (int): frame_id do frame a ser colocado no buffer.
                 frame (ndarray): frame a ser colocado no buffer.
         """
-        if self._old_frame is None:
+        if self._set_frame is not None:
             raise Exception('operação bloqueada até que um novo ciclo ocorra')
-        elif self._old_frame < frame_id and self.qsize() > 0:
-            raise Exception('inconsistencia na operação, onde frame_id é maior que o frame atual ')
+        elif isinstance(self.frame_id, int):
+            if self.frame_id < frame_id and self.qsize() > 0:
+                raise Exception('inconsistencia na operação, onde frame_id é maior que o frame atual ')
+
         self._put((frame_id, frame))
         self._start_frame = False
-        self._old_frame = frame_id
 
     def start(self):
 
         if not self.empty():
             raise VideoBufferError('buffer is not empty')
-        elif self.process is None:
+        elif self.thread is None:
             # Argumentos para a função reader
-            args = (self.path, self.child_conn, self.buffersize, self.bufferlog)
-            process = Process(target=reader, name=self.name, args=args)
-            process.start()
-            self.process = process
-            value = self.parent_conn.recv()
-            if value == READINESS:
-                self.parent_conn.send(True)
+            args = (self.cap,
+                    self.tqueue,
+                    self.child_conn,
+                    event,
+                    self.buffersize,
+                    self.bufferlog)
+            thread = Thread(target=reader, name=self.name, args=args)
+            thread.start()
+            self.thread = thread
+            # ipdb.set_trace()
+        self._checkout()
 
-        self.parent_conn.send((self.start_frame(), self.lot))
+    def join(self):
+        self.parent_conn.send(False)
 
     def read(self):
         # Metodo para o comsumo do buffer, retorna None quando a pilha estiver vazia
+        """
         if not self.empty():
             frame_id, frame = self.get()
-            self.__store_frame_id = frame_id
+            self.frame_id = frame_id
+            self._start_frame = frame_id + 1
+            self._checkout()
             return (frame_id, frame)
         else:
-            if self.start_frame() is False:
-                self.set(self.__store_frame_id + 1)
-            self.start()
-        return None
+        """
+        while self.speed_empty():
+            sleep(0.01)
+        frame_id, frame = self.get()
+        self.frame_id = frame_id
+        self._start_frame = frame_id + 1
+        self._checkout()
+        return (frame_id, frame)
 
+    def _checkout(self):
+        self.check.check_queue(self.queue, self.tqueue)
+        self.check.check_error()
 
+        if len(self.queue) == 0:
+            if self.start_frame() is False and self._set_frame is None:
+                ipdb.set_trace()
+                self.set(self.frame_id + 1)
+            if self._set_frame is not None:
+                self._start_frame = self._set_frame
+                self._set_frame = None
+            self.check.fill_buffer(self.start_frame(), self.last_frame(), self.lot_mapping)
+
+    def finish(self):
+        return self.start_frame() >= self.last_frame()
