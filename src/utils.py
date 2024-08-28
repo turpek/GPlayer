347ed4cb150a9fc00d65e9d8d4f36611d7280a84
Guilherme <gui.faria.96@hotmail.com>
2024-08-28 20:45:11 -0300

    Adqueando src/utils.py para a classe Channel, além disso, foram feitas correções quando o reader chega ao fim dos frames.

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,9 @@ from cv2 import VideoCapture
 from threading import Lock
 from time import time
 from queue import Queue
+from src.channel import Channel
 
 import cv2
-import numpy as np
-import pickle
 import traceback
 
 
@@ -17,9 +16,8 @@ BUFFER_LEFT = 'BUFFER_LEFT'
 
 def reader(cap: VideoCapture,
            queue: Queue,
-           notify: Queue,
-           notify_error: Queue,
-           lock: Lock,
+           conn: Channel,
+           event,
            buffersize: int,
            bufferlog: bool) -> None:
     """Um invólucro que chama a função responsável por ler os frames através do modulo da Opencv.
@@ -30,14 +28,13 @@ def reader(cap: VideoCapture,
             notify (Queue): Queue para fazer a comunicação entre as threads e a linha principal do programa.
             notify_error (Queue): Queue para fazer a comunicação de erros
             buffersize (int): número de frames a serem armazenadas na queue.
-            lock (Lock): Objeto dp modulo threading para ler as filas de forma segura.
             bufferlog (bool): ativa o log.
 
     """
 
     def reader_task(cap: VideoCapture,
                     queue: Queue,
-                    lock: Lock,
+                    conn: Channel,
                     buffersize: int,
                     bufferlog: bool) -> int:
         """Função responsável por ler os frames através do modulo da Opencv.
@@ -45,7 +42,6 @@ def reader(cap: VideoCapture,
             Args:
                 cap (VideoCapture): objeto usado para gerar os frames.
                 queue (Queue): Fila onde os frames serão armazenados.
-                lock (Lock): Objeto dp modulo threading para ler as filas de forma segura.
                 buffersize (int): número de frames a serem armazenadas na queue.
                 bufferlog (bool): ativa o log.
 
@@ -56,7 +52,7 @@ def reader(cap: VideoCapture,
 
         # O fluxo principal do programa deve passar o frame_id "start_frame" que
         # define o  frame incial, ja lot é um set contendo todos os frames a serem lidos.
-        start_frame, lot = queue.get()
+        start_frame, last_frame, lot = conn.recv()
         frame_id, qsize = start_frame, 0
         start = time()
 
@@ -72,20 +68,22 @@ def reader(cap: VideoCapture,
         # Bloco onde os frames são lidos e armazenados na fila
         while True:
 
-            print(frame_id)
             if frame_id in lot:
                 ret, frame = cap.read()
                 if ret:
                     queue.put_nowait((frame_id, frame))
                     qsize += 1
             else:
-                print('grab')
                 cap.grab()
 
             if bufferlog:
                 print(qsize, qsize, frame_id)
             if qsize == buffersize:
                 break
+            elif frame_id == last_frame:
+                break
+            elif frame_id == frame_count:
+                raise IndexError('o video acabou')
             frame_id += 1
 
         if bufferlog:
@@ -107,23 +105,17 @@ def reader(cap: VideoCapture,
             # O pai deve enviar um bool para task_flag, se a mesma for True
             # a task é iniciada, se for false devemos sair do bloco while, se
             # task_flag não for bool, uma exceção deve ser levantada.
-            with lock:
-                (buffer, signal) = notify.get()
-            if buffer is BUFFER_RIGHT:
-                if signal is True:
-                    reader_task(cap, queue, lock, buffersize, bufferlog)
-                elif signal is False:
-                    break
-                else:
-                    raise TypeError('A flag deve ser do tipo bool.')
-            else:
-                with lock:
-                    notify.put((buffer, signal))
+            signal = conn.recv()
+            if signal is True:
+                event.wait()
+                reader_task(cap, queue, conn, buffersize, bufferlog)
+                event.clear()
+            elif signal is False:
+                break
 
     except Exception as e:
         exc_info = traceback.format_exc()
-        with lock:
-            notify_error.put_nowait((e, exc_info))
+        conn.send((e, exc_info))
 
     finally:
         if cap is not None:
