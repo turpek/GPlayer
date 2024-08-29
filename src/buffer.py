58fad9a3099936adceb35ffbf2ce063aa7e8b8c0
Guilherme <gui.faria.96@hotmail.com>
2024-08-29 20:18:29 -0300

    Criação da classe Buffer para servir como buffer na classe VideoBufferLeft.

--- src/buffer.py
+++ src/buffer.py
@@ -1,206 +1,118 @@
-from cv2 import VideoCapture
-from numpy import ndarray
-from src.buffer_error import VideoBufferError
-from pathlib3x import Path
-from threading import Thread, Lock
-from time import time, sleep
-from queue import LifoQueue
-import cv2
-import ipdb
+"""Este módulo tem uma classe que implementa um buffer, onde terá dois
+buffers internos que farão a gestão do objeto, de maneira ao Buffer
+estár sempre cheio, para isso temos os seguintes atributos e métodos.
 
 
-class VideoBufferLeft():
-    def __init__(self, cap, sequence_frames: list[int], buffersize=25, *, bufferlog=False, name='buffer'):
+class Buffer:
+    -primary: É uma Queue que define o buffer primario
+    -secondary: É uma Queue que define o buffer segundario
 
-        # Definições das variaveis que lidam com o Thread
-        self.cap = cap
-        self.name = name
-        self.stack = list()
-        self.buffersize = buffersize
-        self.bufferlog = bufferlog
-        self.sequence_frames_ord = sorted(sequence_frames)
-        self.sequence_frames = dict()
-        self.thread = None
-        self.is_dead = True
-        self.delay = 0.01
+    +secondary_is_empty() Bool: Método que retorna True se o buffer segundario estiver vazio
 
-        # Atributo que monitora os frames lidos na thread
-        self._frame_read = None
-
-        # Definições das variaveis responsavel pela criação do buffer
-        self._end_frame = None
-        self._start_frame = None
-        self.current_frame = None
-
-        # Checando a integridade dos dados
-        self._checking_integrity()
-
-        # Montando a sequencia de frames
-        self._mount_sequence()
-
-    def _checking_integrity(self):
-        if self.end_frame() is None:
-            raise VideoBufferError('the current frame index is not a valid index')
-
-    def _mount_sequence(self):
-
-        end_frame = self.end_frame()
-        self.current_frame = end_frame
-
-        if len(self.sequence_frames_ord) == 0:
-            ...
-        elif end_frame not in self.sequence_frames_ord:
-            self.sequence_frames = {pos: True for pos in self.sequence_frames_ord}
-            self._end_frame = self.sequence_frames_ord[-1]
-        elif end_frame > 0:
-            index = self.sequence_frames_ord.index(end_frame)
-            seqs = self.sequence_frames_ord[:index]
-
-            if index >= self.buffersize:
-                frame0 = index - self.buffersize
-            else:
-                frame0 = 0
-            self.sequence_frames = {pos: True for pos in seqs[frame0: index]}
-        elif end_frame == 0:
-            index = self.sequence_frames_ord.index(end_frame)
-            self.sequence_frames = {self.sequence_frames_ord[index]: True}
-
-    def reflesh(self, sequence_frames):
-        # Metodo que atualiza o buffer
-
-        # Variaveis a serem atualizadas
-        self.stack = list()
-        self.sequence_frames_ord = sorted(sequence_frames)
-        self.sequence_frames = dict()
-        self.thread = None
-        self._end_frame = None
-        self._start_frame = None
-
-        del self.stack
-        # Atualizando o buffer
-        self._checking_integrity()
-        self._mount_sequence()
-
-    def start(self):
-        # Função que carrega os frames na "pilha", a mesma so carregara se a stack esiver vazia
-
-        if not self.empty():
-            return False
-
-        # Inicalizando a obtenção dos estados possiveis dos frames
-        count_frame = self.start_frame()
-        self.cap.set(cv2.CAP_PROP_POS_FRAMES, count_frame)
-
-        def reader(cap, stack, count_frame, sequence_frames, end_frame, bufferlog):
-            # Função responsavel por carregar a stack
-
-            start = time()
-            self._frame_read = count_frame
-            while True:
-                ret, frame = cap.read()
-                if ret and sequence_frames.get(count_frame):
-                    # Deve-se colocar o indice do frame + frame na stack
-                    stack.append((count_frame, frame))
-                if bufferlog:
-                    print(len(stack), count_frame, end_frame)
-                if count_frame == end_frame:
-                    break
-
-                self._frame_read = count_frame
-                count_frame += 1
-
-            if self.current_frame != end_frame:
-                cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_frame + 1)
-
-            end = time()
-            if bufferlog:
-                print(f'\nLidos {len(stack)} em {end - start}s')
-                print(f'{len(stack) / (end - start):.2f} FPS')
-
-        # Argumentos para a função reader
-        args = (self.cap,
-                self.stack,
-                count_frame,
-                self.sequence_frames,
-                self.end_frame(),
-                self.bufferlog)
-        thread = Thread(target=reader, name=self.name, args=args)
-        thread.start()
-        self.thread = thread
-
-    def join(self):
-        self.thread.join()
-
-    def end_frame(self):
-        # Metodo que devolve o ultimo frame a ser empilhado, o mesmo
-        # deve ser calculado como _end_frame = current_frame - 1, como
-        if self._end_frame is None:
-            read_frame = self.cap.get(cv2.CAP_PROP_POS_FRAMES) - 1
-            if read_frame < 0:
-                return None
-            self._end_frame = int(read_frame)
-        return self._end_frame
-
-    def start_frame(self):
-        # Metodo que calcula a posição do primeiro frame a ser lido pela stack
-        if self._start_frame is None:
-            self._start_frame = int(sorted(list(self.sequence_frames.keys()))[0])
-        return self._start_frame
-
-    def current_frame(self):
-        # Calcula a posição atual do frame, se a stack estiver vazia, a mesma
-        # deve retornar None
-        if len(self.stack) == 0:
-            return None
-        return self.start_frame() + len(self.stack)
-
-    def read(self):
-        # Metodo para o comsumo do buffer, retorna None quando a pilha estiver vazia
-        sleep(self.delay)
-        if not self.empty():
-            return self.stack.pop()
-        return None
-
-    # Metodos que simulam alguns metodos do objeto Queue
-
-    def empty(self):
-        # Checa se a pilha esta vazia
-        return len(self.stack) == 0
-
-    def full(self):
-        # Checa se a pilha esta cheia
-        return len(self.stack) == self.buffersize
-
-    def put(self, value: tuple[int, ndarray]):
-        if self.full():
-            _ = self.stack.pop(0)
-        self.stack.append(value)
-
-    def get(self):
-        return self.read()
-
-    def qsize(self):
-        return len(self.stack)
-
-    def maxsize(self):
-        return self.buffersize
-
-    def observer(self, frame_id):
-        # Metod não testado!!
-
-        while True and self.thread.is_alive():
-            if self._frame_read >= frame_id:
-                break
-            sleep(0.0001)
-        return True
+"""
+from queue import Queue
 
 
-if __name__ == '__main__':
-    cap = cv2.VideoCapture('src/model.mp4')
+class Buffer:
+    def __init__(self, size=15):
+        self.__primary = Queue(maxsize=size)
+        self.__secondary = Queue(maxsize=size)
 
-    cap.set(cv2.CAP_PROP_POS_FRAMES, 600)
-    cap.read()
-    buffer = VideoBufferLeft(cap, list(range(1001)), bufferlog=True, buffersize=25)
-    buffer.start()
-    print('Inicalizando...')
-    buffer.join()
+    def __primary_is_empty(self) -> bool:
+        """
+        Método privado que verifica se o buffer primario esta vazio.
+
+        Returns:
+            bool
+        """
+        return self.__primary.empty()
+
+    def __primary_is_full(self) -> bool:
+        """
+        Método privado que verifica se o buffer primario esta cheio.
+
+        Returns:
+            bool
+        """
+        return self.__primary.full()
+
+    def swap(self) -> bool:
+        """
+        Faz a troca dos buffers, se o primario estiver vazio e o segundario não,
+        retornando True em caso de troca e False caso contrário.
+
+        Returns:
+            bool
+        """
+        if self.__primary_is_empty() and self.secondary_is_empty() is False:
+            self.__primary, self.__secondary = self.__secondary, self.__primary
+            return True
+        return False
+
+    def secondary_is_empty(self) -> bool:
+        """
+        Método que verifica se o buffer segundario esta vazio.
+
+        Returns:
+            bool
+        """
+        return self.__secondary.empty()
+
+    def secondary_is_full(self) -> bool:
+        """
+        Método que verifica se o buffer segundario esta cheio.
+
+        Returns:
+            bool
+        """
+        return self.__secondary.full()
+
+    def empty(self) -> bool:
+        """
+        Verifica se o buffer esta cheio.
+
+        Returns:
+            bool
+        """
+        return self.__primary_is_empty() and self.secondary_is_empty()
+
+    def full(self) -> bool:
+        """
+        Verifica se o buffer esta cheio.
+
+        Returns:
+            bool
+        """
+        return self.__primary_is_full() and self.secondary_is_full()
+
+    def put(self, value: any) -> None:
+        """
+        Método para colocar um valor no buffer.
+
+        Args:
+            value any: o valor a ser colocado no buffer.
+
+        Returns:
+            bool
+        """
+        self.__secondary.put_nowait(value)
+
+    def get(self) -> tuple[bool, any]:
+        """
+        Retorna uma tupla contendo o resultado de uma operação de recuperação do buffer.
+
+        A tupla possui dois elementos:
+        - O primeiro elemento (índice 0) é um valor booleano indicando se a operação foi bem-sucedida.
+        - O segundo elemento (índice 1) é o valor armazenado no buffer, que pode ser de qualquer tipo.
+
+        Returns:
+            tuple[bool, any]: Uma tupla onde o primeiro elemento indica o sucesso da operação
+            e o segundo elemento contém o valor recuperado do buffer.
+        """
+
+        if self.__primary_is_empty() is False:
+            return (True, self.__primary.get_nowait())
+        return (False, True)
+
+
+
