4dc4e265959cf7d5519d3f54d1f0092a87f2e929
Guilherme <gui.faria.96@hotmail.com>
2024-09-03 23:29:46 -0300

    Adicionando mais alguns testes para o VideoBufferLeft.

--- tests/test_buffer_left.py
+++ tests/test_buffer_left.py
@@ -1,9 +1,6 @@
 from pytest import fixture
-from pathlib3x import Path
-from queue import Queue
 from src.buffer_left import VideoBufferLeft
-from time import sleep
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 
 import cv2
@@ -66,7 +63,13 @@ def mycap():
 
 @fixture
 def seq():
-    sequence = [(frame_id, np.zeros((2, 2))) for frame_id in range(10, -1, -1)]
+    sequence = [(frame_id, np.zeros((2, 2))) for frame_id in range(10)]
+    return sequence
+
+
+@fixture
+def seq_60():
+    sequence = [(frame_id, np.zeros((2, 2))) for frame_id in range(60, 65)]
     return sequence
 
 
@@ -140,8 +143,8 @@ def test_buffer_VideoBufferLeft_metodo_set_frame_com_lote_nao_linear(myvideo):
 
 
 @pytest.mark.parametrize('myvideo', [(list(range(0, 200, 7)), 25)], indirect=True)
-def test_buffer_VideoBufferRight_metodo_set_frame_com_lote_nao_linear_penultimo_frame(myvideo):
-    expect_start_frame = 14
+def test_buffer_VideoBufferLeft_metodo_set_frame_com_lote_nao_linear_penultimo_frame(myvideo):
+    expect_start_frame = 21
     buffer = myvideo
     buffer.set(195)
     buffer._checkout()
@@ -159,3 +162,53 @@ def test_buffer_VideoBufferLeft_enchendo_o_buffer_manualmente(myvideo, seq):
     result_qsize = buffer.qsize()
     assert result_start_frame == expect_start_frame
     assert result_qsize == expect_qsize
+
+
+@pytest.mark.parametrize('myvideo', [(list(range(100)), 25)], indirect=True)
+def test_buffer_VideoBufferLeft_enchendo_o_buffer_manualmente_com_o_buffer_lotado(myvideo, seq_60):
+    expect_start_frame = False
+    buffer = myvideo
+    buffer.bufferlog = True
+    [buffer.queue.append((frame_id, np.zeros((2, 2)))) for frame_id in range(25, 50)]
+    buffer._old_frame = 25
+    buffer._frame_id = 50
+    [buffer.put(*seq_60.pop(0)) for _ in range(3)]
+    result_start_frame = buffer.start_frame()
+    assert result_start_frame == expect_start_frame
+
+    # Consumindo os frames para testar se start_frame() deixa de estar bloqueado
+    expect_start_frame = 2
+    [buffer.read() for _ in range(25)]
+    try:
+        buffer.read()
+    except TimeoutError:
+        ...
+    result_start_frame = buffer.start_frame()
+    assert result_start_frame == expect_start_frame
+
+
+@pytest.mark.parametrize('myvideo', [(list(range(100)), 25)], indirect=True)
+def test_buffer_VideoBufferLeft_lendo_todo_o_video(myvideo, seq):
+    expect = 99
+    buffer = myvideo
+    buffer.bufferlog = True
+    buffer.set(99)
+    buffer._checkout()
+    buffer.start()
+    buffer._checkout()
+    result = None
+
+    while True:
+        if buffer.finish():
+            break
+        try:
+            ret = buffer.read()
+            ipdb.set_trace()
+            if isinstance(ret, tuple):
+                frame_id, frame = ret
+                print('video', frame_id)
+                result = frame_id
+        except TimeoutError:
+            print(TimeoutError)
+            break
+    assert result == expect
