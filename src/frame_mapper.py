87dd03eeae19ef08c28526c42c7f8c90dcd6122c
Guilherme <gui.faria.96@hotmail.com>
2024-09-25 02:26:05 -0300

    feat: Mapear os id's dos frames através de uma classe
    Criação da classe FrameMapper que faz o mapeamentos dos id's dos frames,
    com ela é possível verificar de forma eficiente se um id está no mapa,
    também é possível adicoinar novos id's e excluir id's antigos.

---
+++ src/frame_mapper.py
@@ -0,0 +1,124 @@
+from array import array
+from copy import copy
+from src.custom_exceptions import InvalidFrameIdError
+import bisect
+
+
+class FrameMapper:
+    """Classe que implementa um mapa que contém os ids de frames de um vídeo."""
+
+    def __init__(self, frame_ids: list[int], frame_count: int) -> None:
+        self.__frame_count = None
+        self.__frame_ids = None
+        self.__mapping = None
+        self.set_mapping(frame_ids, frame_count)
+
+    def __getitem__(self, index: int | slice) -> int | array:
+        """
+        Obtem frame_id's do `FrameMapper` na posições do index, caso não
+        exista um frame_id na posição requerida, um `IndexError` será
+        levantada.
+
+        Args:
+            index (int | slice): valor usado para acessar os ids do `FrameMapper`.
+
+        Returns:
+            int: retorna um inteiro se index for uma instancia de int
+            array: retorna um array contendo os frame_ids, caso index seja uma instancia de slice
+        """
+        return self.__frame_ids[index]
+
+    def __contains__(self, frame_id: int) -> bool:
+        """
+        Verifica se um item está no `FrameMapper`.
+
+        Args:
+            frame_id (int): id do frame da qual se deseja verificar, se está
+                contido no `FrameMapper`.
+
+        Retuns:
+            bool: retorna True caso `frame_id` esteja no conjunto do `FrameMapper`,
+                caso contrário False é retornado.
+        """
+        return frame_id in self.__mapping
+
+    def __get_last_frame_index(self, frame_array: array) -> int:
+        """
+        Método interno que calcula o indice a esquerda do `frame_count`
+        no frame_array, se `frame_count` não estiver presente, o último
+        indice é retornado.
+
+        Args:
+            frame_array (array[int]): um array de inteiros que representa o
+                id dos frames.
+
+        Returns:
+            int: indice à esquerda do `frame_count`, caso o mesmo não esteja no
+                `frame_array`, o último elemento é retornado.
+        """
+        return bisect.bisect_right(frame_array, self.__frame_count - 1)
+
+    def __set_frame_indexes(self, frame_ids: list[int]) -> None:
+        """Método interno para criar um array contendo os frame_ids."""
+        frame_array = array('l', sorted(frame_ids))
+        index = self.__get_last_frame_index(frame_array)
+        self.__frame_ids = frame_array[:index]
+
+    def set_mapping(self, frame_ids: list[int], frame_count: int) -> None:
+        """
+        Define um novo conjunto para mapear os frame_ids.
+
+        Args:
+            frame_ids (list[int]): uma lista que contem os ids dos frames, a quais devem
+                assumir valores inteiros maiores que zero.
+            frame_count (int): número de frames que à mídia em questão tema.
+
+        Returns:
+            None
+        """
+        self.__frame_count = frame_count
+        self.__set_frame_indexes(frame_ids)
+        self.__mapping = set(self.__frame_ids)
+
+    def get_mapping(self) -> set:
+        """
+        Retorna uma copiá do set, que contem os frame_ids contidos no `FrameMapper`.
+
+        Returns:
+            set: um conjunto que contem todos os frame_ids do `FrameMapper`
+        """
+        return self.__mapping.copy()
+
+    def add(self, frame_id: int) -> None:
+        """
+        Adiciona um novo frame_id no `FrameMapper`, desde que o mesmo seja menor que
+        o número de frames da mídia em questão, caso contrário `InvalidFrameIdError` será
+        levantada.
+
+        Args:
+            frame_id (int): indica o id do frame a ser colocado no mapper.
+
+        Returns:
+            None
+        """
+        if frame_id >= self.__frame_count:
+            raise InvalidFrameIdError('frame_id must be less than frame_count')
+        elif frame_id < 0:
+            raise InvalidFrameIdError('frame_id must be greater than zero')
+        bisect.insort(self.__frame_ids, frame_id)
+        self.__mapping.add(frame_id)
+
+    def remove(self, frame_id: int) -> None:
+        """
+        Caso `frame_id` esteja contido em `FrameMapper`, o mesmo será removido,
+        caso contrário nada é feito.
+
+        Args:
+            frame_id (int)> indica o id do frame a ser removido do mapper.
+
+        Returns:
+            None
+        """
+        if frame_id in self.__mapping:
+            del self.__frame_ids[bisect.bisect_left(self.__frame_ids, frame_id)]
+            self.__mapping.remove(frame_id)
