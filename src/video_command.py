5c4cab131b50fc5aa59ede5e1524717a2e1acade
Guilherme <gui.faria.96@hotmail.com>
2024-12-30 19:04:59 -0300

    feat: Adicionado um novo Command além de dois Orchestrator
    Adicionado `RemoveFrameCommando`, responsável para executar a exclusão
    dos frames, através do Orchestrator `FrameRemoveOrchestrator`, que
    manipula os objetos gerados por `PlayerControl`, `FrameMapper` e
    `Trash`, permitindo a integração entre os três objetos, além disso foi
    criado o esqueleto para a implementação do `FrameUndoOrchestrator`, que
    cuidara da restauração dos frames

--- src/video_command.py
+++ src/video_command.py
@@ -1,5 +1,41 @@
 from abc import ABC, abstractmethod
 from src.player_control import PlayerControl
+from src.frame_mapper import FrameMapper
+from src.trash import Trash
+import ipdb
+
+
+class FrameRemoveOrchestrator:
+    def __init__(self, player_control: PlayerControl, frame_mapper: FrameMapper, trash: Trash):
+        self.player_control = player_control
+        self.frame_mapper = frame_mapper
+        self.trash = trash
+
+    def remove(self):
+        player_control = self.player_control
+        swap_buffer = player_control.servant.is_task_complete()
+        frame_id, frame = player_control.remove_frame()
+        self.frame_mapper.remove(frame_id)
+        self.trash.move(frame_id, frame)
+
+        # O swap do buffer deve ocorrer quando o frame a ser removido estiver em alguma das
+        # extremidades (inicio ou final do vídeo) e o buffer estiver na direção da extremidade
+        # em questão, pois ao remover tal frame, o servant passa a ficar vazio, e sua task deve,
+        # ser iniciada na proxima leitura, onde start_frame == end_frame, que no caso é igual ao
+        # primeiro frame_id no buffer master, assim ocorrendo um duplicação de frames.
+        if swap_buffer:
+            player_control.servant, player_control.master = player_control.master, player_control.servant
+
+
+class FrameUndoOrchestrator:
+    def __init__(self, player_control: PlayerControl, frame_mapper: FrameMapper, trash: Trash):
+        self.player_control = player_control
+        self.frame_mapper = frame_mapper
+        self.trash = trash
+
+    def undo(self):
+
+
 
 
 class Command(ABC):
@@ -62,6 +98,14 @@ class PauseDelayCommand(Command):
 
     def executor(self) -> None:
         self.receiver.pause_delay()
+
+
+class RemoveFrameCommand(Command):
+    def __init__(self, receiver: FrameRemoveOrchestrator):
+        self.receiver = receiver
+
+    def executor(self) -> None:
+        self.receiver.remove()
 
 
 class Invoker:
