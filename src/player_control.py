ab933a4a2dd41d4210db0eebb13583fe844a4110
Guilherme <gui.faria.96@hotmail.com>
2025-01-03 20:13:16 -0300

    refactor: Adicionar logs

--- src/player_control.py
+++ src/player_control.py
@@ -1,7 +1,9 @@
+from loguru import logger
 from numpy import ndarray
 from src.buffer_left import VideoBufferLeft
 from src.buffer_right import VideoBufferRight
 from src.video_buffer import IVideoBuffer
+import ipdb
 
 
 class PlayerControl:
@@ -14,6 +16,7 @@ class PlayerControl:
         self.__frame = None
         self.__delay = 35
         self.__default_delay = 35
+        self.old = None
 
     def collect_frame(self) -> None:
         """
@@ -22,8 +25,13 @@ class PlayerControl:
         Returns:
             None
         """
-        if isinstance(self.__frame, ndarray) and not self.servant.is_task_complete():
+        # if isinstance(self.__frame, ndarray) and not self.servant.is_task_complete():
+        if isinstance(self.__frame, ndarray) and self.master[0] != self.frame_id:
+            logger.debug(f'colentando o frame de id {self.frame_id}')
             self.master.put(self.frame_id, self.__frame)
+            if self.servant.is_task_complete():
+                self.frame_id = None
+                self.__frame = None
 
     def __opencv_format(self, frame_id: int, frame: ndarray) -> tuple[bool, ndarray | None]:
         """
@@ -37,6 +45,10 @@ class PlayerControl:
             tuple[bool, ndarray | None]
         """
         if isinstance(frame, ndarray):
+            ls = [x[0] for x in self.servant._buffer._primary]
+            ms = [x[0] for x in self.master._buffer._primary]
+            print('servant', ls[:10])
+            print('master:', ms[:10])
             self.__frame = frame
             self.frame_id = frame_id
             return True, frame
@@ -71,6 +83,7 @@ class PlayerControl:
             None
         """
         if isinstance(self.servant, VideoBufferRight):
+            logger.debug('setando o modo rewind')
             self.servant, self.master = self.master, self.servant
 
     def proceed(self) -> None:
@@ -84,15 +97,18 @@ class PlayerControl:
             None
         """
         if isinstance(self.servant, VideoBufferLeft):
+            logger.debug('setando o modo proceed')
             self.servant, self.master = self.master, self.servant
 
     def set_pause(self):
+        logger.debug(f'setting the pause to {not self.__paused}')
         self.__paused = not self.__paused
 
     def pause(self) -> bool:
         return self.__paused
 
     def set_quit(self):
+        logger.debug('preparando para sair...')
         self.__quit = True
 
     def quit(self) -> bool:
@@ -102,17 +118,19 @@ class PlayerControl:
         if self.__delay > 1:
             self.__delay -= 1
             speed = self.__default_delay / self.__delay
-            print(f'velocidade {speed:.2f}x {self.__delay}')
+            logger.info(f'velocidade {speed:.2f}x {self.__delay}')
 
     def decrease_speed(self) -> None:
         self.__delay += 1
         speed = self.__default_delay / self.__delay
-        print(f'velocidade {speed:.2f}x {self.__delay}')
+        logger.info(f'velocidade {speed:.2f}x {self.__delay}')
 
     def pause_delay(self) -> None:
         if self.__delay == 0:
+            logger.debug('unpause by delay')
             self.__delay = self.__default_delay
         else:
+            logger.debug('pause by delay')
             self.__delay = 0
 
     @property
@@ -120,12 +138,14 @@ class PlayerControl:
         return self.__delay
 
     def remove_frame(self) -> tuple[int | None, ndarray | None]:
+        logger.debug(f'servo {self.servant}')
         if isinstance(self.__frame, ndarray) and not self.pause():
             frame_id, frame = self.frame_id, self.__frame
             self.__frame = None
             self.frame_id = None
             return frame_id, frame
         elif not self.servant.is_task_complete():
-            self.read()
-            return self.remove_frame()
+            frame, ret = self.read()
+            if ret is True:
+                return self.remove_frame()
         return None, None
