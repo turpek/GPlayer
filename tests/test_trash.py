2c797970a164c05a5cf99086a65eaefcf5b1f68e
Guilherme <gui.faria.96@hotmail.com>
2025-01-23 21:11:14 -0300

    test: Adicionado um skip no test `test_trash_com_20_frame_undo`

--- tests/test_trash.py
+++ tests/test_trash.py
@@ -149,7 +149,7 @@ def test_trash_com_11_frame_undo(trash):
     assert expect_frames_id == result_frames_id
 
 
-@pytest.mark.skip(reason='Devo implementar depois')
+# @pytest.mark.skip(reason='Devo implementar depois')
 def test_trash_com_20_frame_undo(trash):
     expect_frames_id = list(range(20, 0, -1))
     [trash.move(frame_id, frame) for frame_id, frame in lote(1, 21)]
