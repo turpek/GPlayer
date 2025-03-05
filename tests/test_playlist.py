c4b4389646d17c913a5b700c9e33395e065f031a
Guilherme T. Faria <gui.faria.96@hotmail.com>
2025-03-04 23:29:22 -0300

    test: Criar testes para os métodos `next_video` `prev_video`

--- tests/test_playlist.py
+++ tests/test_playlist.py
@@ -2,6 +2,8 @@ from src.playlist import Playlist
 from pytest import fixture
 from unittest.mock import MagicMock
 
+import pytest
+
 @fixture
 def play():
     videos = [f'video-{x}.mp4' for x in range(1, 7)]
@@ -23,6 +25,7 @@ def test_video_name_com_1_video_na_lista():
     result = play.video_name()
     assert expect == result
 
+# ################# Testes para o método `next_video`
 
 def test_next_video_lista_vazia():
     videos = []
@@ -67,6 +70,59 @@ def test_next_video_5x_com_lista_de_tamanho_6(play):
     video_player = MagicMock()
 
     [play.next_video(video_player) for _ in range(5)]
+
+    result = video_player.open.call_args.args
+    assert expect in result
+
+
+# ################# Testes para o método `prev_video`
+
+def test_prev_video_lista_vazia():
+    videos = []
+    expect = None
+
+    video_player = MagicMock()
+
+    play = Playlist(videos)
+    play.prev_video(video_player)
+
+    result = video_player.open.call_args.args
+    assert expect in result
+
+def test_prev_video_1x_com_lista_de_tamanho_1():
+    videos = ['video-1.mp4']
+    expect = videos[0]
+
+    video_player = MagicMock()
+
+    play = Playlist(videos)
+    play.prev_video(video_player)
+
+    result = video_player.open.call_args.args
+    assert expect in result
+
+
+def test_prev_video_1x_com_lista_de_tamanho_6(play):
+    expect = 'video-1.mp4'
+
+    video_player = MagicMock()
+
+    # É necessario passar 1 vídeo para a frente para voltar
+    play.next_video(video_player)
+    play.prev_video(video_player)
+
+    result = video_player.open.call_args.args
+    assert expect in result
+
+
+def test_prev_video_5x_com_lista_de_tamanho_6(play):
+    expect = 'video-1.mp4'
+
+    video_player = MagicMock()
+
+    # É necessario passar 5x para a frente para voltar 5x
+    [play.next_video(video_player) for _ in range(5)]
+    [play.prev_video(video_player) for _ in range(5)]
 
     result = video_player.open.call_args.args
     assert expect in result
