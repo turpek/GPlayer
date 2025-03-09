490769d6b0c88181f12093b1271a6cb387345178
Guilherme <gui.faria.96@hotmail.com>
2025-03-09 05:24:43 -0300

    test: Criado mais testes para `SectionManager`

--- tests/test_section.py
+++ tests/test_section.py
@@ -1,17 +1,27 @@
 import pytest
 from collections import deque
-from src.section import ManagerSection, VideoSection
+from src.section import SectionManager, VideoSection
+from src.custom_exceptions import SectionError, SectionIdError
+from pytest import fixture
 import numpy as np
 
 FAKES = {
     'SECTION_IDS': [1, 2, 4],
-    'REMOVED_IDS': [3, 5],
+    'REMOVED_IDS': [5, 3, 6],
     1: {'RANGE_FRAME_ID': (0, 99), 'REMOVED_FRAMES': [14, 13, 12, 11, 10]},
     2: {'RANGE_FRAME_ID': (100, 199), 'REMOVED_FRAMES': [150, 149, 148, 147, 136, 135, 134]},
     3: {'RANGE_FRAME_ID': (202, 299), 'REMOVED_FRAMES': [201, 200]},
     4: {'RANGE_FRAME_ID': (300, 395), 'REMOVED_FRAMES': [396, 397, 398, 399]},
     5: {'RANGE_FRAME_ID': (403, 497), 'REMOVED_FRAMES': [498, 401, 499, 402]},
     6: {'RANGE_FRAME_ID': (500, 599), 'REMOVED_FRAMES': []},
+}
+
+
+FAKES_SEM_REMOVIDOS = {
+    'SECTION_IDS': [1, 2],
+    'REMOVED_IDS': [],
+    1: {'RANGE_FRAME_ID': (0, 99), 'REMOVED_FRAMES': [14, 13, 12, 11, 10]},
+    2: {'RANGE_FRAME_ID': (100, 199), 'REMOVED_FRAMES': [150, 149, 148, 147, 136, 135, 134]},
 }
 
 
@@ -23,6 +33,9 @@ class MockPlayer:
 class MockTrash:
     def __init__(self):
         self._stack = deque()
+
+    def _set_data(self, dados: deque()):
+        self._stack = dados.copy()
 
     def undo(self) -> tuple[bool | np.ndarray]:
         return (True, np.zeros((2, 2)))
@@ -38,11 +51,39 @@ class MockTrash:
 
 class MockFrameMapper:
     def __init__(self, frame_mapper: list):
-        self.__frames_ids = sorted(frame_mapper)
+        self.__frame_ids = sorted(frame_mapper)
 
     def __getitem__(self, index):
-        if len(self.__frames_ids) > 0:
-            return self._frame_ids[index]
+        if len(self.__frame_ids) > 0:
+            return self.__frame_ids[index]
+
+
+def gera_mock(frames_mapping, removed):
+    frame_mapping = sorted(set(frames_mapping) - set(removed))
+    mapping = MockFrameMapper(frame_mapping)
+    trash = MockTrash()
+    trash._set_data(deque(removed))
+    return mapping, trash
+
+
+def gera_mock_com_fake(index):
+    start, end = FAKES[index]['RANGE_FRAME_ID']
+    frame_mapping = list(range(start, end + 1))
+    removed = FAKES[index]['REMOVED_FRAMES']
+    return gera_mock(frame_mapping, removed)
+
+
+@fixture
+def sections():
+    section_manager = SectionManager(FAKES)
+    yield section_manager
+
+
+@fixture
+def mock_config(request):
+    frames_mapping, removed = request.param
+    mapping, trash = gera_mock(frames_mapping, removed)
+    yield mapping, trash
 
 
 def test_VideoSection_start_frame_id_1():
@@ -61,9 +102,8 @@ def test_VideoSection_end_frame_id_1():
     assert expect == result
 
 
-@pytest.mark.skip(reason='despoi vejo')
 def test_VideoSection_removed_frames_id_1():
-    expect = deque([])
+    expect = deque([14, 13, 12, 11, 10])
     secion = VideoSection(FAKES[1])
     result = secion.removed_frames
 
@@ -84,16 +124,375 @@ def test_VideoSection_set_frames_range():
     assert expect_end_frame == result_end_frame
 
 
-def test_ManagerSection_save_data():
-    expect = 1
-    sections = ManagerSection(FAKES)
-    result = sections.current_id
+def test_SectionManager_com_dados_vazios_mas_sem_id():
+    expect = 'there are no sections id to work with'
+    with pytest.raises(SectionIdError) as excinfo:
+        data = {'SECTION_IDS': [], 'REMOVED_IDS': []}
+        SectionManager(data)
+    result = f'{excinfo.value}'
     assert expect == result
 
 
-def test_ManagerSection_next_section():
+def test_SectionManager_com_dados_vazios_mas_com_id():
+    sid = 6
+    expect = f"there is no data for section with id '{sid}'"
+    with pytest.raises(SectionError) as excinfo:
+        data = {'SECTION_IDS': [sid], 'REMOVED_IDS': []}
+        SectionManager(data)
+    result = f'{excinfo.value}'
+    assert expect == result
+
+
+def test_SectionManager_com_dados_vazios_mas_com_id_para_restaurar():
+    sid = 6
+    expect = f"there is no data for section with id '{sid}'"
+    with pytest.raises(SectionError) as excinfo:
+        data = {'SECTION_IDS': [], 'REMOVED_IDS': [sid]}
+        SectionManager(data)
+    result = f'{excinfo.value}'
+    assert expect == result
+
+
+@pytest.mark.parametrize('mock_config', [(list(range(100)), [0, 1, 2, 3, 4, 5])], indirect=True)
+def test_SectionManager_save_data_removendo_os_5_primeiros_frames(sections, mock_config):
+    mapping, trash = mock_config
+    expect_range = (6, 99)
+    expect_removed = [5, 4, 3, 2, 1, 0]
+
+    sections.save_data(mapping, trash)
+    result_range = sections.section_range()
+    result_removed = sections.section_removed()
+
+    assert expect_range == result_range
+    assert expect_removed == result_removed
+
+
+@pytest.mark.parametrize('mock_config', [(list(range(100, 200)), [134, 135, 136, 148, 149, 150])], indirect=True)
+def test_SectionManager_save_data_restaurando_tudo(sections, mock_config):
+    mapping, trash = mock_config
+    expect = []
+
+    trash = MockTrash()
+    trash._set_data(deque())
+    sections.save_data(mapping, trash)
+    result = sections.section_removed()
+
+    assert expect == result
+
+
+@pytest.mark.parametrize('mock_config', [(list(range(200, 300)), [200, 201])], indirect=True)
+def test_SectionManager_save_data_restaurando_o_1o_e_removendo_o_ultimo(sections, mock_config):
+    mapping, trash = mock_config
+    expect_range = (200, 298)
+    expect_removed = [299]
+
+    trash = MockTrash()
+    trash._set_data(deque([299]))
+    mapping = MockFrameMapper(list(range(200, 299)))
+    sections.save_data(mapping, trash)
+    result_range = sections.section_range()
+    result_removed = sections.section_removed()
+
+    assert expect_range == result_range
+    assert expect_removed == result_removed
+
+
+@pytest.mark.parametrize('mock_config', [(list(range(300, 396)), [396, 397, 398, 399])], indirect=True)
+def test_SectionManager_save_data_restaurando_os_ultimos_e_removendo_o_1o(sections, mock_config):
+    mapping, trash = mock_config
+    expect_range = (301, 399)
+    expect_removed = [300]
+
+    trash = MockTrash()
+    trash._set_data(deque([300]))
+    mapping = MockFrameMapper(list(range(301, 400)))
+    sections.save_data(mapping, trash)
+    result_range = sections.section_range()
+    result_removed = sections.section_removed()
+
+    assert expect_range == result_range
+    assert expect_removed == result_removed
+
+
+def test_SectionManager_next_section_1x():
     expect = 2
-    sections = ManagerSection(FAKES)
-    sections.next_section(MockPlayer(), MockFrameMapper([]), MockTrash())
-    result = sections.current_id
+    expect_range = (100, 199)
+
+    sections = SectionManager(FAKES)
+    sections.next_section()
+
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_next_section_2x():
+    expect = 4
+    expect_range = (300, 395)
+
+    sections = SectionManager(FAKES)
+    for _ in range(2):
+        sections.next_section()
+
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_prev_section_no_inicio():
+    expect = 1
+    expect_range = (0, 99)
+
+    sections = SectionManager(FAKES)
+    sections.prev_section()
+
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_next_section_no_final_1x():
+    expect = 2
+    expect_range = (100, 199)
+    sections = SectionManager(FAKES)
+
+    for _ in range(2):
+        sections.next_section()
+    sections.prev_section()
+
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_next_section_no_final_2x():
+    expect = 1
+    expect_range = (0, 99)
+    sections = SectionManager(FAKES)
+
+    for _ in range(2):
+        sections.next_section()
+    for _ in range(2):
+        sections.prev_section()
+
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_next_section_no_final_3x():
+    expect = 1
+    expect_range = (0, 99)
+    sections = SectionManager(FAKES)
+
+    for _ in range(2):
+        sections.next_section()
+    for _ in range(2):
+        sections.prev_section()
+
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_restore_section_sem_nenhuma_secao_excluida():
+    expect = 1
+
+    sections = SectionManager(FAKES_SEM_REMOVIDOS)
+    sections.restore_section()
+    result = sections.section_id
+    assert expect == result
+
+
+def test_SectionManager_restore_section_sem_nenhuma_secao_1x():
+    expect = 6
+    expect_range = (500, 599)
+
+    data = {
+        'SECTION_IDS': [], 'REMOVED_IDS': [6],
+        6: {'RANGE_FRAME_ID': (500, 599), 'REMOVED_FRAMES': []}
+    }
+
+    sections = SectionManager(data)
+    sections.restore_section()
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_restore_section_1x():
+    expect = 5
+    expect_range = (403, 497)
+
+    sections = SectionManager(FAKES)
+    sections.restore_section()
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_restore_section_2x():
+    expect = 3
+    expect_range = (202, 299)
+
+    sections = SectionManager(FAKES)
+    [sections.restore_section() for _ in range(2)]
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_restore_section_sem_nenhuma_secao_2x():
+    expect = 3
+    expect_range = (202, 299)
+
+    data = {
+        'SECTION_IDS': [], 'REMOVED_IDS': [6, 3],
+        6: {'RANGE_FRAME_ID': (500, 599), 'REMOVED_FRAMES': []},
+        3: {'RANGE_FRAME_ID': (202, 299), 'REMOVED_FRAMES': [201, 200]},
+    }
+
+    sections = SectionManager(data)
+    [sections.restore_section() for _ in range(2)]
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_restore_section_3x():
+    expect = 6
+    expect_range = (500, 599)
+
+    sections = SectionManager(FAKES)
+    [sections.restore_section() for _ in range(3)]
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_restore_section_2x_next_1x():
+    expect = 4
+    expect_range = (300, 395)
+
+    sections = SectionManager(FAKES)
+    [sections.restore_section() for _ in range(2)]
+    sections.next_section()
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_restore_section_2x_prev_1x():
+    expect = 2
+    expect_range = (100, 199)
+
+    sections = SectionManager(FAKES)
+    [sections.restore_section() for _ in range(2)]
+    sections.prev_section()
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_restore_section_3x_next_1x():
+    expect = 6
+    expect_range = (500, 599)
+
+    sections = SectionManager(FAKES)
+    [sections.restore_section() for _ in range(3)]
+    sections.next_section()
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_restore_section_3x_prev_2x():
+    expect = 4
+    expect_range = (300, 395)
+
+    sections = SectionManager(FAKES)
+    [sections.restore_section() for _ in range(3)]
+    [sections.prev_section() for _ in range(2)]
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_restore_section_3x_prev_5x():
+    expect = 1
+    expect_range = (0, 99)
+
+    sections = SectionManager(FAKES)
+    [sections.restore_section() for _ in range(3)]
+    [sections.prev_section() for _ in range(5)]
+    result = sections.section_id
+    result_range = sections.section_range()
+
+    assert expect == result
+    assert expect_range == result_range
+
+
+def test_SectionManager_remove_section_vazia():
+    expect = None
+
+    data = {
+        'SECTION_IDS': [], 'REMOVED_IDS': [6, 3],
+        6: {'RANGE_FRAME_ID': (500, 599), 'REMOVED_FRAMES': []},
+        3: {'RANGE_FRAME_ID': (202, 299), 'REMOVED_FRAMES': [201, 200]},
+    }
+    sections = SectionManager(data)
+    sections.remove_section()
+    result = sections.section_id
+
+    assert expect == result
+
+
+def test_SectionManager_remove_section():
+    expect = 2
+
+    sections = SectionManager(FAKES)
+    sections.remove_section()
+    result = sections.section_id
+
+    assert expect == result
+
+
+def test_SectionManager_remove_section_ultimo_secao():
+    expect = 2
+
+    sections = SectionManager(FAKES)
+    [sections.next_section() for _ in range(2)]
+    sections.remove_section()
+    result = sections.section_id
+
     assert expect == result
