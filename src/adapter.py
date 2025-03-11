80c1a214b98ee4c68c56e8b16df3b944d8988752
Guilherme <gui.faria.96@hotmail.com>
2025-03-11 14:09:23 -0300

    feat: Criar um adapter para tratar a entrada de dados sobre uma seção

---
+++ src/adapter.py
@@ -0,0 +1,80 @@
+from abc import ABC, abstractmethod
+from collections import deque
+
+
+class ISectionAdapter(ABC):
+    @abstractmethod
+    def start(self) -> int:
+        ...
+
+    @abstractmethod
+    def end(self) -> int:
+        ...
+
+    @abstractmethod
+    def removed_frames(self) -> deque:
+        ...
+
+    @abstractmethod
+    def black_list_frames(self) -> list:
+        ...
+
+
+class SectionUnionAdapter(ISectionAdapter):
+    def __init__(self, section_1, section_2):
+
+        lower, upper = self.__get_sections(section_1, section_2)
+
+        self.__start = lower.start
+        self.__end = upper.end
+        self.__removed_frames = lower.get_trash() + upper.get_trash()
+        self.__black_list = self.__get_black_list(lower, upper)
+
+    def __get_sections(self, section_1, section_2):
+        if section_2.id_ > section_1.id_:
+            return section_1, section_2
+        return section_2, section_1
+
+    def __get_true_end(self, section):
+        if len(section.get_trash()) > 0:
+            end = max(section.get_trash())
+            return max(section.end, end)
+
+    def __get_black_list(self, lower, upper):
+        neighbor_start = self.__get_true_end(lower) + 1
+        neighbor_end = upper.id_
+        neighborhood = list(range(neighbor_start, neighbor_end))
+        return lower.black_list_frames + neighborhood + upper.black_list_frames
+
+    def start(self) -> int:
+        return self.__start
+
+    def end(self) -> int:
+        return self.__end
+
+    def removed_frames(self) -> deque:
+        return self.__removed_frames
+
+    def black_list_frames(self) -> list:
+        return self.__black_list
+
+
+class FakeSectionAdapter(ISectionAdapter):
+    def __init__(self, data):
+        start, end = data['RANGE_FRAME_ID']
+        self.__start = start
+        self.__end = end
+        self.__removed_frames = deque(data['REMOVED_FRAMES'])
+        self.__black_list = data['BLACK_LIST']
+
+    def start(self) -> int:
+        return self.__start
+
+    def end(self) -> int:
+        return self.__end
+
+    def removed_frames(self) -> deque:
+        return self.__removed_frames
+
+    def black_list_frames(self) -> list:
+        return self.__black_list
