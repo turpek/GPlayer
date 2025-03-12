478265d926f440c8e1c3a120cadb0fc3c544f1d4
Guilherme <gui.faria.96@hotmail.com>
2025-03-12 04:18:48 -0300

    refactor: Renomear `SetionMementoHandler` para `FrameMementoHandler`
    feat: Criar `SectionMementoHandler` para gerenciar remoções de seção.

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ from src.memento import Caretaker
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from src.section import Section
+    from src.section import Section, SectionWrapper
     from src.memento import TrashOriginator
 
 
@@ -16,6 +16,9 @@ class SimpleStack:
     def __init__(self, stack_type):
         self.__stack = deque()
         self.__stack_type = stack_type
+
+    def __len__(self):
+        return len(self.__stack)
 
     def empty(self):
         return len(self.__stack) == 0
@@ -29,6 +32,7 @@ class SimpleStack:
             )
         self.__stack.append(item)
 
+    @property
     def top(self):
         if not self.empty():
             return self.__stack[-1]
@@ -39,7 +43,7 @@ class SimpleStack:
         return self.__stack.pop()
 
 
-class SectionMementoHandler(IMementoHandler):
+class FrameMementoHandler(IMementoHandler):
     def __init__(self, originator: TrashOriginator, caretaker: Caretaker):
         self.__caretaker = caretaker
         self.__originator = originator
@@ -53,4 +57,19 @@ class SectionMementoHandler(IMementoHandler):
         removed_frames = section.get_trash()
         while len(removed_frames) > 0:
             self.__originator.set_state(removed_frames.pop())
+            self.__caretaker.save(self.__originator)
+
+
+class SectionMementoHandler(IMementoHandler):
+    def __init__(self, originator: TrashOriginator, caretaker: Caretaker):
+        self.__caretaker = caretaker
+        self.__originator = originator
+
+    def store_mementos(self, removed_sections: SimpleStack):
+        while self.__caretaker.undo(self.__originator):
+            removed_sections.push(self.__originator.get_state())
+
+    def load_mementos(self, removed_sections: SimpleStack):
+        while not removed_sections.empty():
+            self.__originator.set_state(removed_sections.pop())
             self.__caretaker.save(self.__originator)
