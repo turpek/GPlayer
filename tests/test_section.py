9b20fe0ccb18b1ab8bb4276b38a25a24eaabe00d
Guilherme <gui.faria.96@hotmail.com>
2025-03-14 19:42:01 -0300

    test: Adicionar testes para `next_section` e `prev_section`

--- tests/test_section.py
+++ tests/test_section.py
@@ -2,9 +2,13 @@ import pytest
 from collections import deque
 from src.adapter import FakeSectionAdapter, FakeSectionManagerAdapter
 from src.section import SectionManager, VideoSection
-from src.custom_exceptions import SectionError, SectionManagerError
+from src.custom_exceptions import SectionManagerError
+from src.trash import Trash
 from pytest import fixture
+from threading import Semaphore
+from unittest.mock import patch
 import numpy as np
+import cv2
 
 
 black_list_100 = [200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298]
@@ -1273,3 +1277,47 @@ def test_SectionManager_restaura_caso_geral():
     secman.restore_section()
     secman.restore_section()
     secman.restore_section()
+
+
+# ############## Teste de integração entre o `Trash` e `SectionManager`
+
+def test_SectionManager_carregar_frames_no_memento_do_trash(trash):
+    secman = SectionManager(FakeSectionManagerAdapter(FAKEMAN0), FakeSectionAdapter)
+    secman.load_mementos_frames(trash)
+    assert trash.can_undo()
+
+    expect = 14
+    result, _ = trash.undo()
+    assert expect == result
+
+
+def test_SectionManager_proxima_secao_atualizando_o_trash(trash):
+    secman = SectionManager(FakeSectionManagerAdapter(FAKEMAN0), FakeSectionAdapter)
+    secman.load_mementos_frames(trash)
+
+    secman.next_section(trash)
+    expect = 150
+    result, _ = trash.undo()
+    assert expect == result
+
+
+def test_SectionManager_secao_anterior_atualizando_o_trash(trash):
+    secman = SectionManager(FakeSectionManagerAdapter(FAKEMAN0), FakeSectionAdapter)
+    secman.load_mementos_frames(trash)
+    secman.next_section(trash)
+
+    secman.prev_section(trash)
+    expect = 14
+    result, _ = trash.undo()
+    assert expect == result
+
+
+def test_SectionManager_remover_1o_frame_mover_proxima_secao_e_voltar(trash):
+    secman = SectionManager(FakeSectionManagerAdapter(FAKEMAN0), FakeSectionAdapter)
+    secman.load_mementos_frames(trash)
+    trash.move(0, np.zeros((2, 2)))
+    secman.next_section(trash)
+    secman.prev_section(trash)
+    expect = 0
+    result, _ = trash.undo()
+    assert expect == result
